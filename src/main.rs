d8383766d2b95993aab36359a01ceb12cfe4d45b
Yannick Dorn <yannick.dorn@gmail.com>
2023-01-21 10:33:41 -0600

    started laying out deserializer, still needs work

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-use std::fs::File;
 use std::{fs, collections::HashMap, os::unix::thread};
 use std::io;
-use serde_json;
+use serde_json::{self, json};
 use serde_derive::{Deserialize};
+use clap::Parser;
 use rand::{thread_rng, seq::SliceRandom};
+
+// #[derive(Parser)]
+// #[command(author, version, about, long_about = None)]
+// struct Args {
+// 	#[arg(short, long)]
+// 	from: String,
+// }
 fn main() {
+	// let args = Args::parse();
+
+	let file_name = "./flashcards/simple.json".to_string();
+
+	let data = fs::read_to_string(file_name).unwrap();
+	let deck: serde_json::Value = serde_json::from_str(&data).unwrap();
+
+	for card in deck["cards"].as_array() {
+
+	}
+
 }
 
 
-enum FileType {
-	YAML(String),
-	JSON(String),
-	CSV(String),
-}
+enum FileType { YAML, JSON, CSV }
 
 fn detect_filetype(file_name: &str) -> Result<FileType, String> {
 	let split_file_name = file_name.split('.').last();
 	match split_file_name {
 		Some(suffix) => {
 			match suffix {
-				"yml" => Ok(FileType::YAML("TODO!".to_string())),
-				"json" => Ok(FileType::JSON("TODO!".to_string())),
-				"csv" => Ok(FileType::CSV("TODO!".to_string())),
+				"yml" => Ok(FileType::YAML),
+				"json" => Ok(FileType::JSON),
+				"csv" => Ok(FileType::CSV),
 				_ => Err("No parser available".to_string()),
 			}
 		},
@@ -29,9 +43,25 @@ fn detect_filetype(file_name: &str) -> Result<FileType, String> {
 	}
 }
 
+// the logic for actually assembling the flash cards should be 
+// defined by the application.
 
+
+
+#[derive(Deserialize)]
 struct Deck<T> where T: Card {
-	cards: Vec<T>, 
+	title: String,
+	description: String,
+	cards: Vec<T>
+}
+impl<T:Card> Deck<T> {
+	pub fn new(title: String, description: String, cards: Vec<T> ) -> Deck<T> {
+		Deck {
+			title,
+			description,
+			cards
+		}
+	}
 }
 
 
@@ -40,6 +70,7 @@ trait Card {
 	fn has_tag(&self, tag: &String) -> bool;
 }
 
+#[derive(Deserialize)]
 pub struct MultipleTrue {
 	pub challenge: String,
 	answer: Vec<String>,
@@ -187,6 +218,24 @@ mod test {
 			Ok(_) => assert!(true),
 			_ => assert!(false)
 		}
+	}
+
+	#[test]
+	fn deserialize_json_multiple_card() {
+
+		let json_card: &str = r#"
+			{
+				"challenge": "What is one of the possible square roots of 4?",
+				"answer": ["2", "-2"]
+			}
+		"#;
+
+		let card: Result<MultipleTrue, _> = serde_json::from_str(json_card);
+		match card {
+			Ok(_) => assert!(true),
+			_ => assert!(false)
+		}
+
 	}
 
 }
